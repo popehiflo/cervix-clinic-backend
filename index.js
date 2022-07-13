78043b635d2603ec827c3e823e3ef70c933a571e
popehiflo <poolpetterhijuel@hotmail.com>
2022-07-12 21:46:41 -0500

    feat: install express and server up

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Cervix Clinic Bakend!");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, response to GET request /');
+});
+
+app.listen(8080, () => {
+  console.log('Cervix Clinic Backend Server is running on port 8080');
+});
