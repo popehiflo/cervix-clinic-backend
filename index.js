72da19944af5eb55d800f87c850651b3e54cce16
popehiflo <poolpetterhijuel@hotmail.com>
2022-07-14 09:40:30 -0500

    feat: install morgan, read/parsing body

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(express.json());
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-  res.send('Hello, response to GET request /');
+  res.json('Hello, response to GET request / ✌');
 });
 
 const port = process.env.PORT || 8080;
